ea25ce265b5d30fe50912511d4126f5804599883
faiz <faizsarwar856@gmail.com>
2023-01-05 00:57:20 +0500

    added code

---
+++ index.js
@@ -0,0 +1,134 @@
+"use strict";
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const restService = express();
+
+restService.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+restService.use(bodyParser.json());
+
+restService.post("/echo", function(req, res) {
+  var speech =
+    req.body.result &&
+    req.body.result.parameters &&
+    req.body.result.parameters.echoText
+      ? req.body.result.parameters.echoText
+      : "Seems like some problem. Speak again.";
+  return res.json({
+    speech: speech,
+    displayText: speech,
+    source: "webhook-echo-sample"
+  });
+});
+
+restService.get("/webhook", function(req, res){
+  console.log("jbkjb")
+})
+
+restService.post("/webhook", function(req, res){
+  console.log("jbkjbkkkk")
+  return "hi"
+})
+
+
+restService.post("/audio", function(req, res) {
+  console.log("kjbjn");
+  var speech = "";
+  switch (req.body.result.parameters.AudioSample.toLowerCase()) {
+    //Speech Synthesis Markup Language 
+    case "124":
+      speech =
+        '<speak><audio src="https://osori.direct.quickconnect.to/02_osori/014.mp3">did not get your audio file</audio></speak>';
+      break;
+  }
+  return res.json({
+    speech: speech,
+    displayText: speech,
+    source: "webhook-echo-sample"
+  });
+});
+
+// restService.post("/video", function(req, res) {
+//   return res.json({
+//     speech:
+//       '<speak>  <audio src="https://www.youtube.com/watch?v=VX7SSnvpj-8">did not get your MP3 audio file</audio></speak>',
+//     displayText:
+//       '<speak>  <audio src="https://www.youtube.com/watch?v=VX7SSnvpj-8">did not get your MP3 audio file</audio></speak>',
+//     source: "webhook-echo-sample"
+//   });
+// });
+
+// restService.post("/slack-test", function(req, res) {
+//   var slack_message = {
+//     text: "Details of JIRA board for Browse and Commerce",
+//     attachments: [
+//       {
+//         title: "JIRA Board",
+//         title_link: "http://www.google.com",
+//         color: "#36a64f",
+
+//         fields: [
+//           {
+//             title: "Epic Count",
+//             value: "50",
+//             short: "false"
+//           },
+//           {
+//             title: "Story Count",
+//             value: "40",
+//             short: "false"
+//           }
+//         ],
+
+//         thumb_url:
+//           "https://stiltsoft.com/blog/wp-content/uploads/2016/01/5.jira_.png"
+//       },
+//       {
+//         title: "Story status count",
+//         title_link: "http://www.google.com",
+//         color: "#f49e42",
+
+//         fields: [
+//           {
+//             title: "Not started",
+//             value: "50",
+//             short: "false"
+//           },
+//           {
+//             title: "Development",
+//             value: "40",
+//             short: "false"
+//           },
+//           {
+//             title: "Development",
+//             value: "40",
+//             short: "false"
+//           },
+//           {
+//             title: "Development",
+//             value: "40",
+//             short: "false"
+//           }
+//         ]
+//       }
+//     ]
+//   };
+//   return res.json({
+//     speech: "speech",
+//     displayText: "speech",
+//     source: "webhook-echo-sample",
+//     data: {
+//       slack: slack_message
+//     }
+//   });
+// });
+
+restService.listen( 8000, function() {
+  console.log("Server up and listening");
+});
